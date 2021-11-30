af2f9294de8f487aec796cdbbdef041ecec55438
ErikUmass <evenable@umass.edu>
2021-11-30 10:58:01 -0500

    Fixed mac compilation problem?

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+
+module cs325-team9 {
+
+        requires javafx.graphics;
+        requires javafx.controls;
+        requires javafx.media;
+        requires javafx.base;
+        requires javafx.web;
+        requires javafx.swing;
+        requires javafx.fxml;
+
+        opens application;
+}
