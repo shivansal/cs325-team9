057b766767edc1ae763040028bdcdf62e002d411
ErikUmass <evenable@umass.edu>
2021-12-01 18:06:53 -0500

    .

--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,13 @@
 
-module cs325-team9 {
-
-        requires javafx.graphics;
-        requires javafx.controls;
-        requires javafx.media;
-        requires javafx.base;
-        requires javafx.web;
-        requires javafx.swing;
-        requires javafx.fxml;
-
-        opens application;
-}
+//module cs325-team9 {
+//
+//        requires javafx.graphics;
+//        requires javafx.controls;
+//        requires javafx.media;
+//        requires javafx.base;
+//        requires javafx.web;
+//        requires javafx.swing;
+//        requires javafx.fxml;
+//
+//        opens application;
+//}
